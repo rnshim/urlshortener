27d48760a95d70deb4a239e9315639deeb71dd63
rnshim <ryanshim77@gmail.com>
2023-12-27 22:56:02 -0800

    added enums

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from db import *
 from datetime import datetime
 from hash import *
 from args import *
+import enum
 
 args = get_args()
 
@@ -31,9 +32,9 @@ async def create_url(request: Request):
         print("inserted successfully")
         return {"url":urljson['url'], "alias":alias}
     except ValueError as e:
-        raise HTTPException(status_code=422, detail=str(e))
+        raise HTTPException(status_code=Response.INVALID.status_code, detail=str(e))
     except KeyError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=Response.BAD_REQUEST.status_code, detail=str(e))
     except HTTPException as e:
         print("could not insert")
         return {"message": e.detail}
@@ -58,7 +59,11 @@ def find(alias):
 
     except Exception as e:
         print("url not found")
-        raise HTTPException(status_code=404, detail="Item not found")
+        raise HTTPException(status_code=Response.NOT_FOUND.status_code, detail="Item not found")
+    
+    except HTTPException as e:
+        print("could not find")
+        return {"message": e.detail}
 
 @app.delete("/delete/{alias}")
 def delete(alias: str):
@@ -75,3 +80,25 @@ if __name__ == "__main__":
     uvicorn.run("server:app", host=args.host, port=args.port, reload=True)
 
 #python -m uvicorn server:app --reload
+    
+
+class Response(enum.Enum):
+    OK = (200, "success")
+    NOT_FOUND = (404, "not found")
+    INVALID = (422, "invalid input")
+    BAD_REQUEST = (400, "bad request")
+    CONFLICT = (409, "conflict")
+    INTERNAL_SERVER_ERROR = (500, "internal server error")
+
+    def __init__(self, status_code, message):
+        self.status_code = status_code
+        self.message = message
+
+http_to_enum = {
+    200: Response.OK,
+    404: Response.NOT_FOUND,
+    422: Response.INVALID,
+    400: Response.BAD_REQUEST,
+    409: Response.CONFLICT,
+    500: Response.INTERNAL_SERVER_ERROR
+}
