b6e930749abcdfcd9296794bdee1bc97680aa362
rnshim <ryanshim77@gmail.com>
2024-01-21 22:15:38 -0800

    logging statements

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ from datetime import datetime
 from hash import *
 from args import *
 from response import *
+import logging
+import time
 
 args = get_args()
 
@@ -20,7 +22,8 @@ async def create_url(request: Request):
         if 'alias' in urljson:
             if urljson['alias'].isalnum():
                 insert(DATABASE, urljson['url'], urljson['alias'])
-                print("inserted successfully")
+                #print("inserted successfully")
+                logging.info("inserted successfully")
                 return {"url":urljson['url'], "alias":urljson['alias']}
             else:
                 raise ValueError("Alias must be alphanumeric")
@@ -28,18 +31,23 @@ async def create_url(request: Request):
             raise KeyError("Alias must be provided")
         alias = generate_hash(urljson['url'], str(datetime.now()))
         insert(DATABASE, urljson['url'], alias)
-        print("inserted successfully")
+        #print("inserted successfully")
+        logging.info("inserted successfully")
         return {"url":urljson['url'], "alias":alias}
     except ValueError as e:
+        logging.error(str(e))
         raise HTTPException(status_code=Response.INVALID.code, detail=str(e))
     except KeyError as e:
+        logging.error(str(e))
         raise HTTPException(status_code=Response.BAD_REQUEST.code, detail=str(e))
     except Exception as e:
+        logging.error(str(e))
         raise HTTPException(status_code=Response.INTERNAL_SERVER_ERROR.code, detail=str(e))
 
 @app.get("/list_all")
 def list_all():
     try:
+        logging.info("listing all urls")
         return list_urls(DATABASE)
     except Exception as e:
         return {"message": "Failed to list urls"}
@@ -48,12 +56,14 @@ def list_all():
 def find(alias):
     try: 
         result = retrieve(DATABASE, alias)
-        print("url found successfully")
-        print(result)
+        #print("url found successfully")
+        #print(result)
+        logging.info("url found successfully: "+ result)
         return RedirectResponse(url=result, status_code=308)
 
     except Exception as e:
-        print("url not found")
+        #print("url not found")
+        logging.error("url not found")
         raise HTTPException(status_code=Response.NOT_FOUND.code, detail="Item not found")
 
 @app.delete("/delete/{alias}")
@@ -62,13 +72,16 @@ def delete(alias: str):
         result = retrieve(DATABASE, alias)
         if result is not None:
             delete_url(DATABASE, alias)
-            print("alias deleted successfully")
+            #print("alias deleted successfully")
+            logging.info("alias deleted successfully")
             return {"message": "alias deleted successfully"}
         else:
-            print("alias not found")
+            #print("alias not found")
+            logging.error("alias not found")
             raise HTTPException(status_code=Response.NOT_FOUND.code, detail="Item not found")
     except Exception:
-        print("error during deletion")
+        #print("error during deletion")
+        logging.error("error during deletion")
         raise HTTPException(status_code=Response.NOT_FOUND.code, detail="Item not found")
     
 @app.exception_handler(HTTPException)
@@ -76,6 +89,12 @@ async def http_exception_handler(request, ex):
     enu = http_to_enum.get(ex.status_code, Response.INTERNAL_SERVER_ERROR)
     return HTMLResponse(content=enu.message, status_code=enu.code)
 
+logging.Formatter.converter = time.gmtime
+
+logging.basicConfig(
+    format="%(asctime)s.%(msecs)03dZ %(levelname)s:%(name)s:%(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S",
+)
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host=args.host, port=args.port, reload=True)
